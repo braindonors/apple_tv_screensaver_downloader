7b130a8613bed0eb6094d86618860aba90981911
braindonors <34627596+braindonors@users.noreply.github.com>
2023-07-25 15:49:23 -0700

    Update parse_entries.py - add Python3 Support

--- parse_entries.py
+++ parse_entries.py
@@ -1,3 +1,4 @@
+#!/bin/env python3
 import json
 import re
 import operator
@@ -20,19 +21,19 @@ for item in assets:
     filename = filename + " " + base_filename
     filename_for_url[url] = filename
 
-file = open('urls.csv', "wb")
+file = open('urls.txt', "w")
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
     out_str = "%s,%s" % (x[0],x[1])
     file.write(out_str + "\n")
 file.close()
 
-file = open('urls_aria.txt', "wb")
+file = open('urls_aria.txt', "w")
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
     out_str = "%s\n out=%s" % (x[0],x[1])
     file.write(out_str + "\n")
 file.close()
 
-file = open('filenames.txt',"wb")
+file = open('filenames.txt',"w")
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
     out_str = "%s" % (x[1])
     file.write(out_str + "\n")
